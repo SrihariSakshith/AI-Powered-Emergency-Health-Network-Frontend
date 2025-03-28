a3463579a74917ca4765d9974b18372a6bfe206b
SrihariSakshith <kotichintalasriharisakshith@gmail.com>
2025-03-28 10:10:59 +0530

    updated code

--- src/Login.js
+++ src/Login.js
@@ -24,7 +24,7 @@ const Login = ({ onLoginSuccess }) => {
     try {
       const apiUrl = process.env.REACT_APP_API_BASE_URL || "https://ai-powered-emergency-health-network-server.vercel.app";
 
-      const response = await axios.post(`${apiUrl}/login`, {
+      const response = await axios.post(`${apiUrl}/login/login/login`, {
         username,
         password,
         role,
